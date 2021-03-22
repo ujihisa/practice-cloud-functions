d09fae4cab1a4b8a38251b9db0ef40a968ca5496
ujihisa <ujihisa@users.noreply.github.com>
2021-03-21 17:06:16 -0700

    Pushover

--- draft.rb
+++ draft.rb
@@ -2,8 +2,10 @@
 
 require 'functions_framework'
 require 'json'
+require 'google/cloud/firestore'
+require 'pushover'
 
-def formatted_reservations(date_str, time_want_str)
+def formatted_reservations(date_str, time_notify_str, notify_min_qty, time_list_str)
   require 'net/http'
   require 'json'
   require 'time'
@@ -20,25 +22,48 @@ def formatted_reservations(date_str, time_want_str)
   date = Date.parse(date_str)
   hash = JSON.parse(Net::HTTP.get(URI.parse("https://www.grousemountain.com/products/894/max_available?date=#{date_str}")))
 
+  quantities_by_time =
+    table.to_h {|k, v|
+      [v, hash[k]['qty_rem']]
+    }
+
+  notify_quantities =
+    quantities_by_time.select {|k, v|
+      Time.parse(k) <= Time.parse(time_list_str) &&
+        notify_min_qty <= v
+    }
+  if token = ENV['PUSHOVER_DEVICE_TOKEN']
+    if notify_quantities.empty?
+      # No need to notify
+    else
+      Pushover::Message.new(
+        token: token,
+        user: ENV['PUSHOVER_USER_TOKEN'],
+        message: "#{date_str}\n#{notify_quantities.join("\n").inspect}"
+      ).push
+    end
+  else
+    warn 'Missing PUSHOVER_DEVICE_TOKEN/PUSHOVER_USER_TOKEN'
+  end
+
+
   header = date.strftime('Date: %F (%a)')
   body =
-    table.reject {|_, v|
-      Time.parse(time_want_str) < Time.parse(v)
+    quantities_by_time.select {|k, _|
+      Time.parse(k) <= Time.parse(time_list_str)
     }.map {|k, v|
-      hash[k].then { '  %s  %s' % [v, _1['qty_rem']] }
+      '  %s  %s' % [k, v]
     }
   [header, *body]
 end
 
 FunctionsFramework.http 'index' do |request|
-  require 'google/cloud/firestore'
   if ENV['CREDENTIALS_JSON']
     firestore = ::Google::Cloud::Firestore.new(
       project_id: 'devs-sandbox',
       credentials: JSON.parse(ENV['CREDENTIALS_JSON']),
     )
   else
-    # It's likely asset:precompile. Simply ignore that.
     # Use emulator
     ENV['FIRESTORE_EMULATOR_HOST'] = "firestore-emulator:8080"
     firestore = ::Google::Cloud::Firestore.new(
@@ -49,7 +74,13 @@ FunctionsFramework.http 'index' do |request|
 
   input = JSON.parse request.body.read rescue {}
   date_str = input['date_str'] || '2021-03-22'
-  result = formatted_reservations(date_str, '9:30am').join("\n")
+
+  # Simply skip if it's the past
+  if Date.parse(date_str) < Date.today
+    next "#{date_str} is older than today(#{Date.today}). Skipping."
+  end
+
+  result = formatted_reservations(date_str, '8:30am', 2, '9:30am').join("\n")
 
   col.add({
     date_str: date_str,
